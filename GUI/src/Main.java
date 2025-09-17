64b05e26a26681befa4980cbb175c277f7cbb500
Daniel Ojeda <carlosojeda1700@gmail.com>
2025-09-17 15:50:18 -0600

    first commit

---
+++ GUI/src/Main.java
@@ -0,0 +1,65 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        JFrame frame = new JFrame();
+        Container pane = frame.getContentPane();
+        pane.setLayout(new BorderLayout());
+
+        //Slider to control horizontal rotation
+
+        JSlider headingSlider = new JSlider(0, 360, 180);
+        pane.add(headingSlider, BorderLayout.SOUTH);
+
+        //Slider to control vertical rotation
+        JSlider pitchSlider = new JSlider(SwingConstants.VERTICAL, -90, 90, 0);
+        pane.add(pitchSlider, BorderLayout.EAST);
+
+        //panel to display render results
+        JPanel renderPanel = new JPanel(){
+            public void paintComponent(Graphics g){
+                Graphics2D g2 = (Graphics2D) g;
+                g2.setColor(Color.BLACK);
+                g2.fillRect(0, 0, getWidth(), getHeight());
+            }
+        };
+
+        pane.add(renderPanel, BorderLayout.CENTER);
+        frame.setSize(400, 400);
+        frame.setVisible(true);
+
+
+
+    }
+}
+
+class Vertex {
+    double x;
+    double y;
+    double z;
+
+    Vertex(double x, double y, double z ){
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+}
+
+
+
+class Triangle{
+    Vertex v1;
+    Vertex v2;
+    Vertex v3;
+    Color color;
+
+    Triangle(Vertex  v1, Vertex v2, Vertex v3, Color color){
+        this.v1 = v1;
+        this.v2 = v2;
+        this.v3 = v3;
+        this.color = color;
+    }
+}
